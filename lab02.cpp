6f12ea3db38c4e891526f2efbb7d402391a0fe9c
onigirionta <onigirionta@gmail.com>
2020-10-15 07:09:38 +0300

    added first volume name

--- lab02.cpp
+++ lab02.cpp
@@ -4,9 +4,11 @@
 
 using namespace std;
 
+#define BUFF_SIZE 1024
+
 int main() {
 
-  DWORD bufSize = 2048;
+  DWORD bufSize = BUFF_SIZE;
 
   // info about OS version
   DWORD dwVersion = GetVersion();
@@ -22,17 +24,32 @@ int main() {
 
   // getting system directory
   LPSTR sysInfBuf;
-  GetSystemDirectory(sysInfBuf, bufSize);
+  GetSystemDirectory(sysInfBuf, BUFF_SIZE);
   printf("System directory: %s\n", sysInfBuf);
 
   // getting computer name
-  LPSTR compNameBuf;
+  TCHAR compNameBuf[BUFF_SIZE];
+  bufSize = BUFF_SIZE;
   GetComputerName(compNameBuf, &bufSize);
   printf("Computer name: %s\n", compNameBuf);
 
   // getting user name
-  LPSTR userNameBuf;
-  GetUserName(userNameBuf, &bufSize);
+  // returns null o_o
+  TCHAR userNameBuf[BUFF_SIZE];
+  bufSize = BUFF_SIZE;
+  BOOL res = GetUserName(userNameBuf, &bufSize);
+  if (!res) {
+    DWORD dwError = GetLastError();
+    printf("Error: %d\n", dwError);
+  }
   printf("User name: %s\n", userNameBuf);
+
+  // first volume
+  // returns null o_o
+  TCHAR firstVolumeName[BUFF_SIZE];
+  bufSize = BUFF_SIZE;
+  HANDLE vhandle = FindFirstVolume(firstVolumeName, bufSize);
+  printf("First volume: %s\n", firstVolumeName);
+
   return 0;
 }
